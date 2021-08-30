543e1aa008b09a2685726c428926b0d94d00fffc
Mayur710 <75039133+Mayur710@users.noreply.github.com>
2021-08-30 10:57:14 +0530

    Add files via upload

---
+++ exercise 12.py
@@ -0,0 +1,8 @@
+"""Q)Write a Python program to print the calendar of a given month and year.
+Note : Use 'calendar' module"""
+import calendar
+year = int(input("Please enter your year: "))
+month = int(input("Please enter your month: "))
+print(calendar.month(year, month))
+
+#Success 😎😎😎
